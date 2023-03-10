7107f991b4681a5ae708feffb097041e8426e550
sach1tb <sachitb@gmail.com>
2023-03-10 09:32:16 -0600

    minor edit

--- examples/example05_crank_slider.m
+++ examples/example05_crank_slider.m
@@ -1,20 +1,19 @@
-function example05_crank_slider(crankLength,couplerLength,offset,omega2,configuration, simTime)
+function example05_crank_slider(crankLength,couplerLength,offset,omega2,cfg, simTime)
 
 addpath ../core
 
-% see crankslider.png for the meaning of each parameter
-% parameters >> these can be changed
+% see crankslider.png to visualize the parameters
+
 % link lengths
 if nargin < 1
     crankLength=11; couplerLength=23; offset=-4.5; 
     omega2=2; % rad/s
-    configuration=1;
+    cfg=1; % one for open and 0 for cross
     simTime=2*pi/omega2*2; % multiply by the number of cycles you want to see 
 end
-% *** Processing ***
 
+% *** Processing ***
 t=linspace(0,simTime, 100);
-t1=0; % ground orientation
 t2=omega2*t; % theta_2
 
 
@@ -22,7 +21,7 @@ figure(1); gcf; clf;
 
 for iter=1:numel(t)
     subplot(1,2,1); cla;
-    if configuration
+    if cfg
         [~, t32, ~, d2]=cs_position(crankLength, couplerLength, offset, t2(iter));
     else
         [t32, ~, d2]=cs_position(crankLength, couplerLength, offset, t2(iter));
@@ -45,7 +44,7 @@ subplot(1,2,2);
 % linspace linearly spaces the time into 100 equal sections
 
 [t3c, t3o, dc, do]=cs_position(crankLength, couplerLength, offset, t2);
-if configuration
+if cfg
    plot(t,do, 'k', 'linewidth', 2);
 else
    plot(t,dc, 'k', 'linewidth', 2); 
