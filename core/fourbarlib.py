d64ebd12b8ae53ccbad8ae7ed6d0095f480f7173
sach1tb <sachitb@gmail.com>
2024-09-22 11:41:36 -0500

    more functions

--- core/fourbarlib.py
+++ core/fourbarlib.py
@@ -1,6 +1,57 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+def fourbar_solve(a,b,c,d,t1,t2,cfg):
+    # Define constants $K_1,K_2,K_3,K_4$
+    K1=2*d*c*np.cos(t1)-2*a*c*np.cos(t2)
+    K2=2*d*c*np.sin(t1)-2*a*c*np.sin(t2)
+    K3=2*a*d*np.sin(t1)*np.sin(t2) + 2*a*d*np.cos(t1)*np.cos(t2)
+    K4=b**2-a**2-c**2-d**2
+
+    # Intermediate constants $A,B,C$
+    A=K4+K1+K3
+    B=-2*K2
+    C=K4-K1+K3
+
+    if A.all()==0:
+        A=np.finfo.eps
+
+    t42=2*np.arctan2((-B-np.sqrt(B**2-4*A*C)),(2*A))
+    t41=2*np.arctan2((-B+np.sqrt(B**2-4*A*C)),(2*A))
+
+    K5=2*a*b*np.cos(t2)-2*b*d*np.cos(t1)
+    K6=2*a*b*np.sin(t2)-2*b*d*np.sin(t1)
+    K7=2*a*d*np.sin(t1)*np.sin(t2) + 2*a*d*np.cos(t1)*np.cos(t2)
+    K8=(c**2-d**2-a**2-b**2)
+
+
+    D=K8+K7+K5
+    E=-2*K6
+    F=K8-K5+K7
+
+    # Solve for $\theta_3$ and $\theta_4$. 
+    # Recall that each angle has two solutions, each corresponding to a configuration.
+    t32=2*np.arctan2((-E+np.sqrt(E**2-4*D*F)),(2*D));
+    t31=2*np.arctan2((-E-np.sqrt(E**2-4*D*F)),(2*D));
+    t42=2*np.arctan2((-B+np.sqrt(B**2-4*A*C)),(2*A));
+    t41=2*np.arctan2((-B-np.sqrt(B**2-4*A*C)),(2*A));
+    print("theta_3=%.2f,%.2f degrees" %(t31*180/np.pi,t32*180/np.pi))
+    print("theta_4=%.2f,%.2f degrees" %(t41*180/np.pi,t42*180/np.pi))
+
+    if D.all()==0:
+        D=np.finfo.eps
+
+        # Pick a configuration
+
+    if cfg==1:
+        t3=t31
+        t4=t41
+    else:
+        t3=t32
+        t4=t42
+
+    return t3,t4
+
 def fourbar_plot(a,b,c,d,t1,t2,t3,t4,phi,ap):
     Rz=np.array([0,0])
     if np.isscalar(t1):
